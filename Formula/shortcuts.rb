633487fc8596759e2a7300f257b7ea56d1a4d7c8
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-09-23 13:15:57 +0700

    Update shortcuts (1.1.1 & 2.0.0)

--- Formula/shortcuts.rb
+++ Formula/shortcuts.rb
@@ -1,13 +1,18 @@
 class Shortcuts < Formula
   desc "CLI manager for your text replacements on macOS"
   homepage "https://github.com/rodionovd/shortcuts"
-  url "https://github.com/rodionovd/shortcuts/archive/v1.1.0.tar.gz"
-  sha256 "aa74d875256b4fe440b96a8b4c13a4615f7085257b2da1e8c2f1cbfd912334cc"
-
   head "https://github.com/rodionovd/shortcuts.git"
 
   depends_on :xcode => :build
   depends_on :macos => :el_capitan
+  
+  if MacOS.release <= :el_capitan
+    url "https://github.com/rodionovd/shortcuts/archive/v1.1.1.tar.gz"
+    sha256 "be38d1e0f21de25f2695853b75d5ec9d4686e3adcf0e41b9278c6d728b6d224e"
+  else
+    url "https://github.com/rodionovd/shortcuts/archive/v2.0.0.tar.gz"
+    sha256 "630bc293831bb0dad94c677286746f7ee9a9c3933234c945e09a350f775add6f"
+  end
 
   def install
     xcodebuild "-target", "shortcuts", "-configuration", "Release", "SYMROOT=symroot", "OBJROOT=objroot", "CODE_SIGN_IDENTITY="
